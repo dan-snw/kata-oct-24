161ad938307a14819e2dbd9e6090862335306838
Dan Snow <dan.snow@fundapps.co>
2024-10-24 11:44:45 +0100

    :recycle: Refactor logic

--- Kata/Main/ChangeCalculator.cs
+++ Kata/Main/ChangeCalculator.cs
@@ -4,9 +4,21 @@ public static class ChangeCalculator
 {
     public static List<int> GetChange(int changeNeeded, List<int> denominations)
     {
+        var returnList = new List<int>();
         if (changeNeeded == 1)
         {
-          return new List<int>() {1,0,0,0,0,0,0,0};
+          for(int i = 0; i < denominations.Count; i++)
+          {
+            if (denominations[i] == 1)
+            {
+                returnList.Add(1);
+            }
+            else
+            {
+                returnList.Add(0);
+            }
+          }
+          return returnList;
         }
         return new List<int>() {0,0,0,0,0,0,0,0};
 

c167e5bc05b10cace964306693248006757e79b7
Dan Snow <dan.snow@fundapps.co>
2024-10-24 11:33:01 +0100

    :white_check_mark: Add test of 1

--- Kata/Main/ChangeCalculator.cs
+++ Kata/Main/ChangeCalculator.cs
@@ -4,6 +4,11 @@ public static class ChangeCalculator
 {
     public static List<int> GetChange(int changeNeeded, List<int> denominations)
     {
+        if (changeNeeded == 1)
+        {
+          return new List<int>() {1,0,0,0,0,0,0,0};
+        }
         return new List<int>() {0,0,0,0,0,0,0,0};
+
     }
 }
--- Kata/Tests/ChangeCalculatorTests.cs
+++ Kata/Tests/ChangeCalculatorTests.cs
@@ -16,4 +16,18 @@ public class ChangeCalculatorTests
         var result = ChangeCalculator.GetChange(changeNeeded, denominations);
         result.Count.Should().Be(length);
     }
+
+    [Fact]
+    public void ChangeCalculator_For1Needed_Returns1ThenZeroes()
+    {
+        var denominations = new List<int> { 1, 2, 5, 10, 20, 50, 100, 200 };
+        const int changeNeeded = 1;
+
+        var length = denominations.Count;
+
+        var result = ChangeCalculator.GetChange(changeNeeded, denominations);
+        result.Count.Should().Be(length);
+        result.Should().BeEquivalentTo(new List<int>() {1,0,0,0,0,0,0,0});
+    }
+
 }

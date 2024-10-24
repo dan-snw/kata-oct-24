931ad72964348b2ca1e3c78738ce325a06cd4300
Dan Snow <dan.snow@fundapps.co>
2024-10-24 11:51:09 +0100

    :recycle: Back to for loop for easier future coding

--- Kata/Main/ChangeCalculator.cs
+++ Kata/Main/ChangeCalculator.cs
@@ -2,5 +2,20 @@
 
 public static class ChangeCalculator
 {
-    public static List<int> GetChange(int changeNeeded, List<int> denominations) => denominations.Select(t => t == changeNeeded ? 1 : 0).ToList();
+    public static List<int> GetChange(int changeNeeded, List<int> denominations)
+    {
+        var returnList = new List<int>();
+        for(var i = denominations.Count - 1; i >= 0; i--)
+        {
+            if (denominations[i] == changeNeeded)
+            {
+                returnList.Add(1);
+            }
+            else
+            {
+                returnList.Add(0);
+            }
+        }
+        return returnList;
+    }
 }

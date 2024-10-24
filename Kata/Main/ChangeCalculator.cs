0d79f9dc6aa8daaefd4daa60e0422d5fbf166d32
Dan Snow <dan.snow@fundapps.co>
2024-10-24 11:47:16 +0100

    :white_check_mark: Add test of single coin needed

--- Kata/Main/ChangeCalculator.cs
+++ Kata/Main/ChangeCalculator.cs
@@ -2,25 +2,5 @@
 
 public static class ChangeCalculator
 {
-    public static List<int> GetChange(int changeNeeded, List<int> denominations)
-    {
-        var returnList = new List<int>();
-        if (changeNeeded == 1)
-        {
-          for(int i = 0; i < denominations.Count; i++)
-          {
-            if (denominations[i] == 1)
-            {
-                returnList.Add(1);
-            }
-            else
-            {
-                returnList.Add(0);
-            }
-          }
-          return returnList;
-        }
-        return new List<int>() {0,0,0,0,0,0,0,0};
-
-    }
+    public static List<int> GetChange(int changeNeeded, List<int> denominations) => denominations.Select(t => t == changeNeeded ? 1 : 0).ToList();
 }
--- Kata/Tests/ChangeCalculatorTests.cs
+++ Kata/Tests/ChangeCalculatorTests.cs
@@ -30,4 +30,18 @@ public class ChangeCalculatorTests
         result.Should().BeEquivalentTo(new List<int>() {1,0,0,0,0,0,0,0});
     }
 
+    [Fact]
+    public void ChangeCalculator_For2Needed_Returns1ThenZeroes()
+    {
+        var denominations = new List<int> { 1, 2, 5, 10, 20, 50, 100, 200 };
+        const int changeNeeded = 2;
+
+        var length = denominations.Count;
+
+        var result = ChangeCalculator.GetChange(changeNeeded, denominations);
+        result.Count.Should().Be(length);
+        result.Should().BeEquivalentTo(new List<int>() {0,1,0,0,0,0,0,0});
+    }
+
+
 }

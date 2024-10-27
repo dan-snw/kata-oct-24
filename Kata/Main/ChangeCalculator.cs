eda8693cb6778dd2d09fbab3880d99e2586cad41
Dan Snow <danjsnow@outlook.com>
2024-10-27 11:41:23 +0000

    :recycle: Add base case to method

--- Kata/Main/ChangeCalculator.cs
+++ Kata/Main/ChangeCalculator.cs
@@ -4,6 +4,10 @@ public static class ChangeCalculator
 {
     public static int[] GetChange(int changeNeeded, int[] denominations)
     {
+        if (denominations.Length == 1)
+        {
+            return [changeNeeded / denominations[0]];
+        }
         var returnList = new List<int>();
         foreach (int coin in denominations.Reverse())
         {

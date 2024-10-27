f4975b72afbd93ae1a72789a5f0a1c9c69e43922
Dan Snow <danjsnow@outlook.com>
2024-10-27 09:48:21 +0000

    :recycle: Change to inline tests

--- Kata/Tests/ChangeCalculatorTests.cs
+++ Kata/Tests/ChangeCalculatorTests.cs
@@ -6,38 +6,9 @@ namespace Tests;
 public class ChangeCalculatorTests
 {
     [Theory]
-    [InlineData(0, new[] { 1, 2, 5, 10, 20, 50, 100, 200 })]
-    public void ChangeCalculator_ForZeroNeeded_ReturnsListOfZerosRightLength(int changeNeeded, int[] denominations)
-    {
-        var length = denominations.Length;
-
-        var result = ChangeCalculator.GetChange(changeNeeded, denominations);
-        result.Count.Should().Be(length);
-    }
-
-    [Fact]
-    public void ChangeCalculator_For1Needed_Returns1ThenZeroes()
-    {
-        var denominations = new[] { 1, 2, 5, 10, 20, 50, 100, 200 };
-        const int changeNeeded = 1;
-
-        var length = denominations.Length;
-
-        var result = ChangeCalculator.GetChange(changeNeeded, denominations);
-        result.Count.Should().Be(length);
-        result.Should().BeEquivalentTo(new List<int>() {1,0,0,0,0,0,0,0});
-    }
-
-    [Fact]
-    public void ChangeCalculator_For2Needed_Returns1ThenZeroes()
-    {
-        var denominations = new[] { 1, 2, 5, 10, 20, 50, 100, 200 };
-        const int changeNeeded = 2;
-
-        var length = denominations.Length;
-
-        var result = ChangeCalculator.GetChange(changeNeeded, denominations);
-        result.Count.Should().Be(length);
-        result.Should().BeEquivalentTo(new List<int>() {0,1,0,0,0,0,0,0});
-    }
+    [InlineData(0, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 0, 0, 0, 0, 0, 0, 0, 0 })]
+    [InlineData(1, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 1, 0, 0, 0, 0, 0, 0, 0 })]
+    [InlineData(2, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 0, 1, 0, 0, 0, 0, 0, 0 })]
+    public void ChangeCalculator_ShouldReturnCorrectChange(int changeNeeded, int[] denominations, int[] expected) =>
+        ChangeCalculator.GetChange(changeNeeded, denominations).Should().BeEquivalentTo(expected);
 }

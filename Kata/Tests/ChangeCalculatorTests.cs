2e845cd3f2c274ffe847a42ebe20f530f8b18eda
Dan Snow <danjsnow@outlook.com>
2024-10-27 10:33:56 +0000

    :recycle: Split out spec tests, use readonly Denominations

--- Kata/Tests/ChangeCalculatorTests.cs
+++ Kata/Tests/ChangeCalculatorTests.cs
@@ -5,15 +5,21 @@ namespace Tests;
 
 public class ChangeCalculatorTests
 {
+    private static readonly int[] Denominations = [ 1, 2, 5, 10, 20, 50, 100, 200 ];
+    
     [Theory]
-    [InlineData(0, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 0, 0, 0, 0, 0, 0, 0, 0 })]
-    [InlineData(1, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 1, 0, 0, 0, 0, 0, 0, 0 })]
-    [InlineData(2, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 0, 1, 0, 0, 0, 0, 0, 0 })]
-    [InlineData(3, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 1, 1, 0, 0, 0, 0, 0, 0 })]
-    [InlineData(4, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 0, 2, 0, 0, 0, 0, 0, 0 })]
+    [InlineData(0, new [] { 0, 0, 0, 0, 0, 0, 0, 0 })]
+    [InlineData(1, new [] { 1, 0, 0, 0, 0, 0, 0, 0 })]
+    [InlineData(2, new [] { 0, 1, 0, 0, 0, 0, 0, 0 })]
+    [InlineData(3, new [] { 1, 1, 0, 0, 0, 0, 0, 0 })]
+    [InlineData(4, new [] { 0, 2, 0, 0, 0, 0, 0, 0 })]
+    public void ChangeCalculator_ShouldReturnCorrectChange(int changeNeeded, int[] expected) =>
+        ChangeCalculator.GetChange(changeNeeded, Denominations).Should().BeEquivalentTo(expected);
+   
+    [Theory]
     [InlineData(15, new[] { 1, 2, 5, 10, 20, 50 }, new [] { 0, 0, 1, 1, 0, 0 })]
     [InlineData(45, new[] { 1, 2, 5, 10, 20, 50 }, new [] { 0, 0, 1, 0, 2, 0 })]
     [InlineData(121, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 1, 0, 0, 0, 1, 0, 1, 0 })]
-    public void ChangeCalculator_ShouldReturnCorrectChange(int changeNeeded, int[] denominations, int[] expected) =>
+    public void ChangeCalculator_SpecTests(int changeNeeded, int[] denominations, int[] expected) =>
         ChangeCalculator.GetChange(changeNeeded, denominations).Should().BeEquivalentTo(expected);
 }

73ba18f5a714678ba8876c6cd2f98c7cd57a72d2
Dan Snow <danjsnow@outlook.com>
2024-10-27 10:04:22 +0000

    :white_check_mark: Add final spec test

--- Kata/Tests/ChangeCalculatorTests.cs
+++ Kata/Tests/ChangeCalculatorTests.cs
@@ -13,6 +13,7 @@ public class ChangeCalculatorTests
     [InlineData(4, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 0, 2, 0, 0, 0, 0, 0, 0 })]
     [InlineData(15, new[] { 1, 2, 5, 10, 20, 50 }, new [] { 0, 0, 1, 1, 0, 0 })]
     [InlineData(45, new[] { 1, 2, 5, 10, 20, 50 }, new [] { 0, 0, 1, 0, 2, 0 })]
+    [InlineData(121, new[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new [] { 1, 0, 0, 0, 1, 0, 1, 0 })]
     public void ChangeCalculator_ShouldReturnCorrectChange(int changeNeeded, int[] denominations, int[] expected) =>
         ChangeCalculator.GetChange(changeNeeded, denominations).Should().BeEquivalentTo(expected);
 }

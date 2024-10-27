8d1a79c0e3e5d88310dcce404478939b8aeb3007
Dan Snow <danjsnow@outlook.com>
2024-10-27 12:07:40 +0000

    :recycle: Use range operator not Take()

--- Kata/Main/ChangeCalculator.cs
+++ Kata/Main/ChangeCalculator.cs
@@ -5,7 +5,7 @@ public static class ChangeCalculator
     public static int[] GetChange(int changeNeeded, int[] denominations) =>
         denominations.Length == 1 
             ? [changeNeeded / denominations[0]] 
-            : GetChange(changeNeeded % denominations[^1], denominations.Take(denominations.Length - 1).ToArray())
+            : GetChange(changeNeeded % denominations[^1], denominations[..^1])
                 .Concat([changeNeeded / denominations[^1]])
                 .ToArray();
 }

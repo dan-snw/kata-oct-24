3d3d9d64a9054c81b539b1e4cf068d617c5b5e42
Dan Snow <danjsnow@outlook.com>
2024-10-27 11:34:25 +0000

    :white_check_mark: Make two base case tests

--- Kata/Tests/ChangeCalculatorTests.cs
+++ Kata/Tests/ChangeCalculatorTests.cs
@@ -7,9 +7,11 @@ public class ChangeCalculatorTests
 {
     private static readonly int[] Denominations = [ 1, 2, 5, 10, 20, 50, 100, 200 ];
    
-    [Fact]
-    public void ChangeCalculator_BaseRecursionCase_ShouldReturnCorrectChange() =>
-        ChangeCalculator.GetChange(1, new[] { 1 }).Should().BeEquivalentTo(new[] { 1 });
+    [Theory]
+    [InlineData(0, new [] { 0 })]
+    [InlineData(1, new [] { 1 })]
+    public void ChangeCalculator_BaseRecursionCase_ShouldReturnCorrectChange(int changeNeeded, int[] expected) =>
+        ChangeCalculator.GetChange(changeNeeded, new[] { 1 }).Should().BeEquivalentTo(expected);
     
     [Theory]
     [InlineData(0, new [] { 0, 0, 0, 0, 0, 0, 0, 0 })]

4de58c04e8421cce95f2fb5c86428ecfeead0214
Dan Snow <danjsnow@outlook.com>
2024-10-27 12:04:38 +0000

    :recycle: Tidy with ternary operator

--- Kata/Main/ChangeCalculator.cs
+++ Kata/Main/ChangeCalculator.cs
@@ -2,15 +2,10 @@
 
 public static class ChangeCalculator
 {
-    public static int[] GetChange(int changeNeeded, int[] denominations)
-    {
-        if (denominations.Length == 1)
-        {
-            return [changeNeeded / denominations[0]];
-        }
-        var returnList = new List<int>();
-        returnList.AddRange(GetChange(changeNeeded % denominations[^1], denominations.Take(denominations.Length - 1).ToArray()));
-        returnList.Add(changeNeeded / denominations[^1]);
-        return returnList.ToArray();
-    }
+    public static int[] GetChange(int changeNeeded, int[] denominations) =>
+        denominations.Length == 1 
+            ? [changeNeeded / denominations[0]] 
+            : GetChange(changeNeeded % denominations[^1], denominations.Take(denominations.Length - 1).ToArray())
+                .Concat([changeNeeded / denominations[^1]])
+                .ToArray();
 }

2787c5f1cbb0f2d3cc52de3c875f5c626a452270
Dan Snow <danjsnow@outlook.com>
2024-10-27 11:08:14 +0000

    :recycle: Remove a few unnecessary lines

--- Kata/Main/ChangeCalculator.cs
+++ Kata/Main/ChangeCalculator.cs
@@ -2,17 +2,15 @@
 
 public static class ChangeCalculator
 {
-    public static List<int> GetChange(int changeNeeded, int[] denominations)
+    public static int[] GetChange(int changeNeeded, int[] denominations)
     {
-        var reversedDenominations = denominations.Reverse().ToArray();
         var returnList = new List<int>();
-        foreach (int coin in reversedDenominations)
+        foreach (int coin in denominations.Reverse())
         {
-            var numberOfThisCoinNeeded = changeNeeded / coin;
-            returnList.Add(numberOfThisCoinNeeded);
+            returnList.Add(changeNeeded / coin);
             changeNeeded %= coin;
         }
         returnList.Reverse();
-        return returnList;
+        return returnList.ToArray();
     }
 }
